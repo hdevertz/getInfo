e1498db883640ea7094302a263742783070b429e
hdevertz <159195340+hdevertz@users.noreply.github.com>
2024-02-07 09:05:30 -0500

    Update getInfo.ps1

--- getInfo.ps1
+++ getInfo.ps1
@@ -9,34 +9,30 @@ $folder = "$env:UserProfile\Desktop\tracker"
 $compsys = Get-WmiObject win32_computersystem
 
 #----------
-#MONITOR INFORMATION
-#----
-Get-CimInstance -Namespace root/wmi -ClassName wmimonitorid | Select-Object @{Name="MonitorName";Expression={
-    if ($_.UserFriendlyNameLength -eq 0) {
-        "None"
-    }
-    else {
-        ($_.UserFriendlyName -ne 0 | foreach { [char]$_}) -join ""
-    }
-}}, @{Name="MonitorSerial";Expression={
-    if ($_.SerialNumberIDLength -eq 0) {
-        "N/A"
-    }
-    else {
-        ($_.SerialNumberID -ne 0 | foreach { [char]$_}) -join ""
-    }
-}} | Out-File $folder\"monitor.txt"
+#FILE CHECK
+#-----
+$driveroot = Get-PsDrive -PsProvider FileSystem | ForEach-Object {$_.Root} | Get-ChildItem
+$drivewindows = Get-PsDrive -PsProvider FileSystem | ForEach-Object {"$($_.Root)Windows"} | Get-ChildItem | Where-Object {($_.Name -like "*.txt") -or ($_.Name -like "*.dll")}
+$cachedbroker = Get-ChildItem C:\Users\$env:username\AppData\Local\Packages\Microsoft.AAD.BrokerPlugin_cw5n1h2txyewy
+$cachedidentity = Get-ChildItem C:\Users\$env:username\AppData\Local\Microsoft\IdentityCache -Recurse
+$cachedoneauth = Get-ChildItem C:\Users\$env:username\AppData\Local\Microsoft\OneAuth -Recurse
+#-----
+$filecheck = "`nFILE CHECK",
+"`ndriveroot`n--------------------", $driveroot,
+"`ndrivewindows`n--------------------", $drivewindows,
+"`ncachedbroker`n--------------------", $cachedbroker,
+"`ncachedidentity`n--------------------", $cachedidentity,
+"`ncachedoneauth`n--------------------", $cachedoneauth
+$filecheck | Out-File $folder\"filecheck.txt"
 #-----
 #----------
-
-
 
 
 
 #----------
 #EMAIL OUTPUT
 #-----
-$Body = (Get-Content $folder\"monitor.txt" -Raw)
+$Body = (Get-Content $folder\"filecheck.txt" -Raw)
 $mail = "mail." + $compsys.Domain
 Write-Host $Body
 Send-MailMessage -SmtpServer $mail -To "hdesai@evertz.com" -From "Reports@5288.IT" -Body $Body -Subject $compsys.DnsHostname
